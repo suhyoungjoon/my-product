9420a9c194eed611e2d84311e672c1f92d3f1d1c
SKCC <SKCC@SKCC18D00135.SKCC.NET>
2020-04-07 17:13:27 +0900

    first commit

---
+++ src/main/java/com/example/demo/OrderPlaced.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+public class OrderPlaced {
+    String eventType;
+    Long orderId;
+    int qty;
+    Long productId;
+    String productName;
+
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+}
